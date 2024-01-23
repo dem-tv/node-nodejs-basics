5d367f89f03b2b509dbcfbcac2f0ebf205f5f105
tatyana demchuk <tatuana.demchuk@gmail.com>
2024-01-24 02:28:28 +0300

    feat: implement workers an cp

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import {getFileData} from "../utils.js";
+import {Worker} from "node:worker_threads";
+import os from 'os';
+import path from "path";
+
 const performCalculations = async () => {
-    // Write your code here
+  const {__dirname} = getFileData(import.meta.url);
+  const cpuCores = os.cpus().length;
+  const promises = new Array(cpuCores).fill(0).map((el, i) => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(path.resolve(__dirname, 'worker.js'), {
+        workerData: 10 + i,
+      });
+      worker.on('message', (data) => {
+        // reject()
+        resolve(data)
+      });
+      worker.on('error', reject);
+      worker.on('exit', (code) => {
+        if (code !== 0) reject(new Error(`Worker stopped with exit code ${code}`));
+      });
+    })
+  })
+
+  Promise.allSettled(promises).then(data => {
+    console.log('data', data.map(({status, value}) => {
+      return {
+        status: status === 'fulfilled' ? 'resolved' : 'error', value: value === undefined ? null : value
+      }
+    }))
+  })
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,40 @@
+import {
+  Worker, isMainThread, parentPort, workerData,
+} from 'node:worker_threads'
+import {getFileData} from "../utils.js";
+import path from "path";
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
+// This function sends result of nthFibonacci computations to main thread
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  const {__dirname} = getFileData(import.meta.url);
+  if (isMainThread) {
+    const workerPromise = new Promise((resolve, reject) => {
+      const worker = new Worker(path.resolve(__dirname, 'worker.js'), {
+        workerData: 20,
+      });
+      worker.on('message', resolve);
+      worker.on('error', reject);
+      worker.on('exit', (code) => {
+        if (code !== 0)
+          reject(new Error(`Worker stopped with exit code ${code}`));
+      });
+    })
+
+    workerPromise.then((data) => {
+      console.log(`${data.fibonacciN}th Fibonacci number - ${data.fibonacciResult}`)
+    }).catch(err => {
+      console.log(err)
+    });
+  } else {
+    parentPort.postMessage({
+        fibonacciResult: nthFibonacci(workerData),
+        fibonacciN: workerData,
+      }
+    )
+  }
 };
 
 sendResult();
